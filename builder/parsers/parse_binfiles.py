130804ce75d42aa3171dc0ad56fededacb747640
EG <egun@antisigma.classics.utoronto.ca>
2016-12-31 11:32:25 -0500

    perl -pi -w -e 's/Copyright: E Gunderson 2016/Copyright: E Gunderson 2016-17/g;' */*.py

--- builder/parsers/parse_binfiles.py
+++ builder/parsers/parse_binfiles.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 	HipparchiaBuilder: generate a database of Greek and Latin texts
-	Copyright: E Gunderson 2016
+	Copyright: E Gunderson 2016-17
 	License: GPL 3 (see LICENSE in the top level directory of the distribution)
 """
 
