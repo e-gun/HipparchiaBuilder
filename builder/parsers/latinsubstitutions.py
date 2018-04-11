2f89a62c82c77d2cf3050211958f9d23a4de0caa
EG <egun@antisigma.classics.utoronto.ca>
2018-04-11 18:56:06 -0400

    perl -pi -w -e 's/Copyright: E Gunderson 2016-17/Copyright: E Gunderson 2016-18/g;' */*.py

--- builder/parsers/latinsubstitutions.py
+++ builder/parsers/latinsubstitutions.py
@@ -1,3 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+	HipparchiaBuilder: compile a database of Greek and Latin texts
+	Copyright: E Gunderson 2016-18
+	License: GNU GENERAL PUBLIC LICENSE 3
+		(see LICENSE in the top level directory of the distribution)
+"""
+
 import re
 
 
